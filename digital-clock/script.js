2946d94479d01d02a23d12fcd16055b8e76f75c0
hija-happy <hijahappy2004@gmail.com>
2025-03-05 16:18:06 +0530

    digital clock

--- digital-clock/script.js
+++ digital-clock/script.js
@@ -5,12 +5,21 @@ function showTime(){
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
-    var session ='PM';
+    var session ='AM';
 
-    if(h=12)
-        session = 'AM';
-        
+    if(h>12)
 
+    if(h==0){
+        h==12;
+    }
+    if(h>12)
+    {
+        h=h-12;
+        session ='PM';
+    }
+    s = (s<10)?'0'+s:s;
+    m = (m<10)?'0'+m:m;
+    h = (h<10)?'0'+h:h;
 
 document.getElementById('digitalClock').innerHTML = h +" : "+ m +" : "+s+" "+session;
 setTimeout(showTime,1000);
