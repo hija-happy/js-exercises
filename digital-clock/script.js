c156cd28ea65d6a96d78168ea1bfa2aad457e72d
hija-happy <hijahappy2004@gmail.com>
2025-03-05 15:54:10 +0530

    First Commit

---
+++ digital-clock/script.js
@@ -0,0 +1,20 @@
+
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    var session ='PM';
+
+    if(h=12)
+        session = 'AM'
+    if(h>12)
+        
+
+
+document.getElementById('digitalClock').innerHTML = h +" : "+ m +" : "+s+" "+session;
+setTimeout(showTime,1000);
+}
+
+showTime();
